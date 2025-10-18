54a8fe34574bf87d73fda25bc3347f0b9487f5f2
JonasCGN <jonasbo66@gmail.com>
2025-10-18 11:10:04 -0300

    feat: adicionar componente TopBar e ajustar importações nas páginas

--- frontend/src/app/pages/home/index.tsx
+++ frontend/src/app/pages/home/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import TopBar from '../../components/TopBar';
+import TopBar from './TopBar';
 import BottomBar from './BottomBar';
 import Content from './Content';
 
 export default function HomePage() {
   return (
     <main>
-    <TopBar background_image="/assets/images/fundo_top_bottom.png"/>
+    <TopBar/>
     <Content />
     <BottomBar />
     </main>
