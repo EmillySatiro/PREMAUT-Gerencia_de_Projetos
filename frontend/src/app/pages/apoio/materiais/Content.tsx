ef052b45ac1f31a1e402c40fe295adc9413c377e
JonasCGN <jonasbo66@gmail.com>
2025-10-25 14:46:30 -0300

    feat(imagens, ícones): atualizar caminhos de imagens e ícones para constantes

--- frontend/src/app/pages/apoio/materiais/Content.tsx
+++ frontend/src/app/pages/apoio/materiais/Content.tsx
@@ -1,55 +1,57 @@
 "use client";
 
 import Colors from '@/app/components/color';
-
 import React from 'react';
+
+import Image from '../../../components/assets/images';
+import Icons from '../../../components/assets/icons';
 
 export const MateriaisApoio: React.FC = () => {
     const [search, setSearch] = React.useState('');
 
     const livros: { urlImage: string; name: string; qtdPag: string }[] = [
         {
-            urlImage: '/assets/images/capa_livro.jpg',
+            urlImage: Image.capaLivro,
             name: 'Livro de atividades 1',
             qtdPag: '20 páginas',
         },
         {
-            urlImage: '/assets/images/capa_livro.jpg',
+            urlImage: Image.capaLivro,
             name: 'Livro de atividades 2',
             qtdPag: '15 páginas',
         },
         {
-            urlImage: '/assets/images/capa_livro.jpg',
+            urlImage: Image.capaLivro,
             name: 'Livro de atividades 3',
             qtdPag: '30 páginas',
         },
         {
-            urlImage: '/assets/images/capa_livro.jpg',
+            urlImage: Image.capaLivro,
             name: 'Livro de atividades 4',
             qtdPag: '25 páginas',
         },
         {
-            urlImage: '/assets/images/capa_livro.jpg',
+            urlImage: Image.capaLivro,
             name: 'Livro de atividades 4',
             qtdPag: '25 páginas',
         },
         {
-            urlImage: '/assets/images/capa_livro.jpg',
+            urlImage: Image.capaLivro,
             name: 'Livro de atividades 4',
             qtdPag: '25 páginas',
         },
         {
-            urlImage: '/assets/images/capa_livro.jpg',
+            urlImage: Image.capaLivro,
             name: 'Livro de atividades 4',
             qtdPag: '25 páginas',
         },
         {
-            urlImage: '/assets/images/capa_livro.jpg',
+            urlImage: Image.capaLivro,
             name: 'Livro de atividades 4',
             qtdPag: '25 páginas',
         },
         {
-            urlImage: '/assets/images/capa_livro.jpg',
+            urlImage: Image.capaLivro,
             name: 'Livro de atividades 4',
             qtdPag: '25 páginas',
         },
@@ -222,7 +224,7 @@ export const MateriaisApoio: React.FC = () => {
                         display: 'flex',
                         alignItems: 'center',
                     }}>
-                        <img src="/assets/images/lupa.svg" alt="Lupa" className="w-6 h-6" />
+                        <img src={Icons.lupa} alt="Lupa" className="w-6 h-6" />
                     </span>
                 </div>
             </div>
