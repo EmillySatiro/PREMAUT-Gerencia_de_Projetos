d821d39d67cecbffbdc3f9fc3000601629516577
JonasCGN <jonasbo66@gmail.com>
2025-10-25 15:33:28 -0300

    Refactor authentication and registration pages to use LogoCompleta component and update image imports
    - Updated 'Esqueci_a_senha', 'Login', and 'Redefinir_senha' pages to replace static logo images with LogoCompleta component.
    - Improved code formatting and consistency across these pages.
    - Refactored 'CadastroEvento', 'CadastroPaciente', 'RelatorioEscrever', 'EditarPaciente', 'EditarPerfil', and 'EditarPublicacao' pages to use new image and icon imports.
    - Enhanced user interface elements by ensuring consistent use of icons and images throughout the application.

--- frontend/src/app/pages/cadastro/evento/index.tsx
+++ frontend/src/app/pages/cadastro/evento/index.tsx
@@ -2,7 +2,11 @@
 
 import React, { useState } from "react";
 import TopBar from '@/app/components/TopBar';
+
 import "./styles.css";
+
+import Image from "@/app/components/assets/images";
+import Icons from "@/app/components/assets/icons";
 
 export default function CadastroEvento() {
     const [data, setData] = useState("");
@@ -23,99 +27,98 @@ export default function CadastroEvento() {
         >
             {/* TopBar fixada no topo */}
             <div style={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 1000 }}>
-            <TopBar background_image="/assets/images/fundo_somos.jpg" />
+                <TopBar background_image={Image.fundoSomos} />
             </div>
 
             <div
-            className="relatorio-content"
-            style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                // Espaço para a TopBar fixa (ajuste o valor se a TopBar tiver outra altura)
-                paddingTop: 80,
-            }}
+                className="relatorio-content"
+                style={{
+                    flex: 1,
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingTop: 80,
+                }}
             >
-            <button
-                className="back-button"
-                type="button"
-                onClick={() => window.history.back()}
-            >
-                <img
-                src="/assets/images/mdi_arrow-up.svg"
-                alt="Voltar"
-                width="54"
-                height="54"
-                />
-            </button>
-            <div style={{ marginTop: "50px", marginBottom: "50px" }}>
-            <h1 className="titulo">Cadastro de Evento</h1>
-            </div>
-
-            <div className="campo">
-                <label htmlFor="titulo">Titulo</label>
-                <input
-                id="titulo"
-                type="text"
-                placeholder="Digite o título"
-                value={titulo}
-                onChange={(e) => setTitulo(e.target.value)}
-                />
-            </div>
-
-            <form className="form" onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", flex: 1 }}>
-                <div className="linha">
-                <div className="campo metade">
-                    <label htmlFor="data">Data</label>
-                    <input
-                    id="data"
-                    type="date"
-                    value={data}
-                    onChange={(e) => setData(e.target.value)}
+                <button
+                    className="back-button"
+                    type="button"
+                    onClick={() => window.history.back()}
+                >
+                    <img
+                        src={Icons.mdi_arrow_back}
+                        alt="Voltar"
+                        width="54"
+                        height="54"
                     />
-                </div>
-
-                <div className="campo metade">
-                    <label htmlFor="criador">Criador do evento</label>
-                    <input
-                    id="criador"
-                    type="text"
-                    placeholder="Nome do criador"
-                    value={criador}
-                    onChange={(e) => setCriador(e.target.value)}
-                    />
-                </div>
+                </button>
+                <div style={{ marginTop: "50px", marginBottom: "50px" }}>
+                    <h1 className="titulo">Cadastro de Evento</h1>
                 </div>
 
                 <div className="campo">
-                <label htmlFor="localizacao">Localização</label>
-                <input
-                    id="localizacao"
-                    type="text"
-                    placeholder="Digite o local"
-                    value={localizacao}
-                    onChange={(e) => setLocalizacao(e.target.value)}
-                />
+                    <label htmlFor="titulo">Titulo</label>
+                    <input
+                        id="titulo"
+                        type="text"
+                        placeholder="Digite o título"
+                        value={titulo}
+                        onChange={(e) => setTitulo(e.target.value)}
+                    />
                 </div>
 
-                <div className="campo" style={{ flex: 1 }}>
-                <label htmlFor="descricao">Descrição</label>
-                <textarea
-                    id="descricao"
-                    rows={4}
-                    placeholder="Descreva o evento"
-                    value={descricao}
-                    onChange={(e) => setDescricao(e.target.value)}
-                    style={{ height: "100%" }}
-                />
-                </div>
+                <form className="form" onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+                    <div className="linha">
+                        <div className="campo metade">
+                            <label htmlFor="data">Data</label>
+                            <input
+                                id="data"
+                                type="date"
+                                value={data}
+                                onChange={(e) => setData(e.target.value)}
+                            />
+                        </div>
 
-                <div className="actions">
-                <button className="submit-button" type="submit">
-                    CADASTRAR
-                </button>
-                </div>
-            </form>
+                        <div className="campo metade">
+                            <label htmlFor="criador">Criador do evento</label>
+                            <input
+                                id="criador"
+                                type="text"
+                                placeholder="Nome do criador"
+                                value={criador}
+                                onChange={(e) => setCriador(e.target.value)}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="campo">
+                        <label htmlFor="localizacao">Localização</label>
+                        <input
+                            id="localizacao"
+                            type="text"
+                            placeholder="Digite o local"
+                            value={localizacao}
+                            onChange={(e) => setLocalizacao(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="campo" style={{ flex: 1 }}>
+                        <label htmlFor="descricao">Descrição</label>
+                        <textarea
+                            id="descricao"
+                            rows={4}
+                            placeholder="Descreva o evento"
+                            value={descricao}
+                            onChange={(e) => setDescricao(e.target.value)}
+                            style={{ height: "100%" }}
+                        />
+                    </div>
+
+                    <div className="actions">
+                        <button className="submit-button" type="submit">
+                            CADASTRAR
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     );
--- frontend/src/app/pages/editar/perfil/index.tsx
+++ frontend/src/app/pages/editar/perfil/index.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import TopBar from '@/app/components/TopBar';
 import "./styles.css";
+import Icons from "@/app/components/assets/icons";
 
 export default function EditarPerfil() {
   const [nome, setNome] = useState("");
@@ -16,27 +17,23 @@ export default function EditarPerfil() {
     console.log({ nome, genero, telefone, email, nascimento });
   };
 
-
   return (
     <div className="editar-perfil-container">
-     
-    <div
-      className="topbar-wrapper"
-    >
-      <img src="/assets/images/solar_heart-broken.svg" alt="Coração" className="h-10 w-10" />
-      <span className="premautTitle" style={{ marginLeft: 8 }}>PREMAUT</span>
-    </div>
 
+      <div
+        className="topbar-wrapper"
+      >
+        <img src={Icons.solarHeartBroken} alt="Coração" className="h-10 w-10" />
+        <span className="premautTitle" style={{ marginLeft: 8 }}>PREMAUT</span>
+      </div>
 
       <div className="perfil-card">
-      <button className="voltar" onClick={() => window.history.back()}>
-        <img src="/assets/images/mdi_arrow-up.svg" alt="Voltar" width="32" />
-      </button>
+        <button className="voltar" onClick={() => window.history.back()}>
+          <img src={Icons.mdi_arrow_back} alt="Voltar" width="32" />
+        </button>
 
-
-      
         <div className="icone-perfil">
-          <img src="/assets/images/mdi_user.svg" alt="Perfil" />
+          <img src={Icons.mdi_user} alt="Perfil" />
         </div>
 
         <form className="form-perfil" onSubmit={handleSubmit}>
@@ -98,7 +95,7 @@ export default function EditarPerfil() {
             Salvar
           </button>
 
-         
+
         </form>
       </div>
     </div>
