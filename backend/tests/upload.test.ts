4bcb2d8ee56379f3d40a8a465968d842f1726027
JonasCGN <jonasbo66@gmail.com>
2025-11-13 13:11:49 -0300

    chore: remoção de seções de código não utilizadas

--- backend/tests/upload.test.ts
+++ backend/tests/upload.test.ts
@@ -2,22 +2,29 @@ import request from 'supertest';
 import app from '../src/index';
 import path from 'path';
 
-describe('Upload API', () => {
+describe('Materiais API', () => {
   let uploadedFileName = 'pato.jpg';
 
-  it('deve fazer upload de um arquivo', async () => {
+  it('deve fazer materiais de um arquivo', async () => {
     const filePath = path.join(__dirname, uploadedFileName);
     const res = await request(app)
-      .post('/api/upload')
+      .post('/api/materiais')
       .attach('file', filePath);
     expect(res.statusCode).toBe(201);
     expect(res.body.url).toBeDefined();
   });
 
+  it('deve pegar todos os materiais', async () => {
+    const res = await request(app)
+      .get('/api/materiais');
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+
   it('deve atualizar (substituir) um arquivo', async () => {
     const filePath = path.join(__dirname, uploadedFileName);
     const res = await request(app)
-      .put(`/api/upload/${uploadedFileName}`)
+      .put(`/api/materiais/${uploadedFileName}`)
       .attach('file', filePath);
     expect(res.statusCode).toBe(200);
     expect(res.body.url).toBeDefined();
@@ -25,12 +32,12 @@ describe('Upload API', () => {
 
   it('deve remover um arquivo existente', async () => {
     const res = await request(app)
-      .delete(`/api/upload/${uploadedFileName}`);
+      .delete(`/api/materiais/${uploadedFileName}`);
     expect(res.statusCode).toBe(204);
   });
 
   it('deve retornar 404 ao remover um arquivo inexistente', async () => {
-    const res = await request(app).delete('/api/upload/arquivo_inexistente.jpg');
+    const res = await request(app).delete('/api/materiais/arquivo_inexistente.jpg');
     expect([204, 404]).toContain(res.statusCode);
   });
 });
